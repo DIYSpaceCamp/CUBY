5b879ee2cf5b85c1488b960254981dc163ea498f
Mohammad Arif Siddiqui <mohammadarif.siddiqui@telusinternational.com>
2020-09-09 16:02:39 +0530

    Updates

---
+++ precache-manifest.e26599c5ba42f14e58c9c1d9b7077e5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131486cbc3d14858f5831549963bdfef",
+    "url": "/CUBY/index.html"
+  },
+  {
+    "revision": "c21c13d08e36eb84cd46",
+    "url": "/CUBY/static/css/main.404ba9cc.chunk.css"
+  },
+  {
+    "revision": "bce190c73b05da864571",
+    "url": "/CUBY/static/js/2.f0447e44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CUBY/static/js/2.f0447e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21c13d08e36eb84cd46",
+    "url": "/CUBY/static/js/main.b622a4f7.chunk.js"
+  },
+  {
+    "revision": "54f29723dbd10583d6c4",
+    "url": "/CUBY/static/js/runtime-main.dcac3b78.js"
+  },
+  {
+    "revision": "42df509ca4cb4cbd797e4956a94ae488",
+    "url": "/CUBY/static/media/cuby.42df509c.webm"
+  },
+  {
+    "revision": "a57ca0ec5fa2ecd7dc1f93d5370bd08c",
+    "url": "/CUBY/static/media/icon1.a57ca0ec.jpg"
+  },
+  {
+    "revision": "d47bffce9ab28ace2a176515a1ae2c6d",
+    "url": "/CUBY/static/media/logo.d47bffce.png"
+  }
+]);
