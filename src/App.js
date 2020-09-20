a9de20e74bd157c70758c9cbc02dc78fa2782afd
Mohammad Arif Siddiqui <mohammadarif.siddiqui@telusinternational.com>
2020-09-20 16:40:48 +0530

    outreach page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Homepage from './containers/Homepage/homepage'
+import Homepage from './containers/Homepage/homepage';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Outreach from './pages/outreach/outreach';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/outreach" component={Outreach} />
+      </Router>
     </div>
   );
 }
