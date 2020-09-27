0ccee506f884b3bf9aa6d8407cbd125ca77afa1b
DIYSpaceCamp <70541999+DIYSpaceCamp@users.noreply.github.com>
2020-09-27 14:26:11 +0530

    routing working on github page (#6)
    Co-authored-by: Mohammad Arif Siddiqui <mohammadarif.siddiqui@telusinternational.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import Homepage from './containers/Homepage/homepage';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, HashRouter } from 'react-router-dom'
 import Outreach from './pages/outreach/outreach';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/outreach" component={Outreach} />
-      </Router>
-    </div>
+    <HashRouter basename="/">
+      <div className="App">
+        
+          <Route exact path="/" component={Homepage} />
+          <Route path="/outreach" component={Outreach} />
+        
+      </div>
+    </HashRouter>
   );
 }
 
